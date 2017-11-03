3d7c21dd9469e04529accd65c9cf16c81579e12e
Imputes <zhuzi@MacBook-Air.local>
2017-11-03 20:11:02 +0800

    Create core data stack and model

---
+++ Nike Collection/Core Data Configuration/CoreDataStack.swift
@@ -0,0 +1,37 @@
+//
+//  CoreDataStack.swift
+//  Nike Collection
+//
+//  Created by Shao Kahn on 11/3/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "NikeCollectionMomd")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
